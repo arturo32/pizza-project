2b3abcfcaee9a692ae12299fa816d85c1a76d315
Arturo Fonseca <arturofonsecas32@gmail.com>
2023-08-30 00:29:44 -0300

    Add files

---
+++ pizza-frontend/src/app/slice/slice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'slice',
+  templateUrl: './slice.component.html',
+  styleUrls: ['./slice.component.css']
+})
+export class SliceComponent {
+
+}
